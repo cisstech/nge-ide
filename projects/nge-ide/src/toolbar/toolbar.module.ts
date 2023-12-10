878cfa89ebb434c3d0f955754e80edccf928454a
Mamadou Cisse <mciissee@gmail.com>
2023-12-10 16:54:23 +0100

    fix(toolbar): wrong style

--- projects/nge-ide/src/toolbar/toolbar.module.ts
+++ projects/nge-ide/src/toolbar/toolbar.module.ts
@@ -6,11 +6,13 @@ import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 import { ToolbarComponent } from './toolbar.component';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @NgModule({
   imports: [
     CommonModule,
 
+    NzMenuModule,
     NzButtonModule,
     NzDividerModule,
     NzDropDownModule,
